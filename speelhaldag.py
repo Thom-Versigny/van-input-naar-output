759ddbe8770bfb5bdcbf979385c72f4b7d96ceb7
Thom Versigny <thomversigny@gmail.com>
2021-09-09 11:19:23 +0200

    added code and proof

---
+++ speelhaldag.py
@@ -0,0 +1,9 @@
+toegang = float(3 * 7.45)
+vr = float(0.37 * 27)
+total = 32.34
+print("3 x 7.45 + 0.37 x 27")
+print("=")
+print(toegang + vr)
+if toegang + vr == total:
+	print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 32.34 euro")
+input("Press enter to exit ;)")
