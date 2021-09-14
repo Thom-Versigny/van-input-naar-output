cfc86f3861bb0e867ab162c5322245c4017e4b6b
Thom Versigny <thomversigny@gmail.com>
2021-09-14 09:41:29 +0200

    code edited

--- feestlunch.py
+++ feestlunch.py
@@ -2,10 +2,5 @@ crossant = float(17 * 0.39)
 stokbrood = float(2 * 2.78)
 kortingbon = float(3 * 0.50)
 total = crossant + stokbrood - kortingbon
-b = 10.69
-print("17 x 0.39 + 2 x 2.78 - 3 x 0.50")
-print("=")
-print(total)
-if total == b:
-  print("De feestlunch kost je bij de bakker 10.69 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
+print("De feestlunch kost je bij de bakker",total,"voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
 input("Press enter to exit ;)")
--- speelhaldag.py
+++ speelhaldag.py
@@ -1,9 +1,5 @@
 toegang = float(3 * 7.45)
 vr = float(0.37 * 27)
-total = 32.34
-print("3 x 7.45 + 0.37 x 27")
-print("=")
-print(toegang + vr)
-if toegang + vr == total:
-	print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 32.34 euro")
+total = toegang + vr
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar",total,)
 input("Press enter to exit ;)")
