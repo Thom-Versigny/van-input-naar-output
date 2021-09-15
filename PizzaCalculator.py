0542e0b22d735939429b265845d2288ce6fc3ce8
Thom Versigny <thomversigny@gmail.com>
2021-09-15 12:47:32 +0200

    code edited

--- PizzaCalculator.py
+++ PizzaCalculator.py
@@ -1,28 +1,21 @@
 #Thom Versigny pizza calculator
-small = 1
-medium = 2
-large = 3
 input('Choose a pizza, press enter to continu')
 print(""" ----------------------------------------------------
 |  Pizza small  : $5.99
 |  Pizza medium : $7.99
 |  pizza large  : $9.99
  ----------------------------------------------------""")
-size = int(input('Choose the size: small press 1, medium press 2, large press 3: '))
-muchpizza = int(input('How many pizzas do you want? '))
-if size == small and muchpizza <= 10:
-	print('you chose ',muchpizza,', small pizzas that will cost you:',5.99 * muchpizza,'$')
-	input("Press enter to pay")
+small = int(input('How many small pizzas do you want? '))
+medium = int(input('How many medium pizzas do you want? '))
+large = int(input('How many large pizzas do you want? '))
+total = 5.99 * small + 7.99 * medium + 9.99 * large
 
-if size == medium and muchpizza <= 10:
-	print('you chose ',muchpizza,', medium pizzas that will cost you:',7.99 * muchpizza,'$')
+if small <= 10 and medium <= 10 and large <= 10:
+	print(f'you chose {small}, small pizzas cost: ${5.99 * small}')
+	print(f'you chose {medium}, medium pizzas cost: ${7.99 * medium}')
+	print(f'you chose {large}, large pizzas cost: ${9.99 * large}')
+	print(f'that will cost in total: ${total}')
 	input("Press enter to pay")
-
-if size == large and muchpizza <= 10:
-	print('you chose ',muchpizza,', large pizzas that will cost you:',9.99 * muchpizza,'$')
-	input("Press enter to pay")
-
-if size > large or muchpizza > 10:
-	print("Sorry, we dont deliver that kind/number of pizza")
+else:
+	print('We dont deliver that number of pizzas please stay under the 10')
 	input('press enter to exit')
-
